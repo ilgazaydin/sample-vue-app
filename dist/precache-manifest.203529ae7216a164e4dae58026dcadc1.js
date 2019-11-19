949043b7663a2aa812b163352adb021798d2ae97
Ilgaz <ilgaz@Zeren-MacBook-Air.local>
2019-11-19 05:03:34 +0000

    dist folder added

---
+++ dist/precache-manifest.203529ae7216a164e4dae58026dcadc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5aaba9355a5898f5394",
+    "url": "/sample-vue-app/css/app.89d8f829.css"
+  },
+  {
+    "revision": "b14cd1c7a4e5f61909c815a89eaca413",
+    "url": "/sample-vue-app/index.html"
+  },
+  {
+    "revision": "a5aaba9355a5898f5394",
+    "url": "/sample-vue-app/js/app.d370316f.js"
+  },
+  {
+    "revision": "eacf337474665e11f20f",
+    "url": "/sample-vue-app/js/chunk-vendors.0a891057.js"
+  },
+  {
+    "revision": "3b74f2c63389ceba11a4078ad6e75f6b",
+    "url": "/sample-vue-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sample-vue-app/robots.txt"
+  }
+]);
